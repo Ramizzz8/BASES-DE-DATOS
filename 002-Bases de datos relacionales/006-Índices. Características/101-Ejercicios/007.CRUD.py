2df754da5e32cb65677c3c1b9f15f04c80403950
Ramizzz8 <ajulianrague@gmail.com>
2025-10-17 13:06:51 +0200

    1

--- 002-Bases de datos relacionales/006-Índices. Características/101-Ejercicios/007.CRUD.py
+++ 002-Bases de datos relacionales/006-Índices. Características/101-Ejercicios/007.CRUD.py
@@ -4,7 +4,7 @@ cursor = conexion.cursor()
 print("Programa agenda SQLite v0.1 Andres Ramirez")
 
 while True:
-    print("Escoge una opcion:\n1.-Crear cliente\n2.-Listar Clientes")
+    print("Escoge una opcion:\n1.-Crear cliente\n2.-Listar Clientes\n3.-Actualizar clientes\n4.-Eliminar clientes\n5.-SALIR")
     opcion = int(input("Selecciona una opcion: "))
     if opcion == 1:
         nombre = input("Introduce el nombre del cliente: ")
@@ -12,7 +12,10 @@ while True:
         email = input("Introduce el email del cliente: ")
         cursor.execute("""
             INSERT INTO clientes VALUES(
-                NULL, '"""+nombre+"""','"""+apellidos+"""','"""+email+"""'
+                NULL, 
+                '"""+nombre+"""',
+                '"""+apellidos+"""',
+                '"""+email+"""'
             );
             """)
         conexion.commit()
@@ -20,9 +23,30 @@ while True:
         cursor.execute('''
             SELECT * FROM clientes;
         ''')
-
         filas = cursor.fetchall()
-
         for fila in filas:
             print(fila)
+    
+    elif opcion == 3:
+        identificador = input("Introduce el identificador a actualizar: ")
+        nombre = input("Introduce el nombre del cliente: ")
+        apellidos = input("Introduce los apellidos del cliente: ")
+        email = input("Introduce el email del cliente: ")
+        cursor.execute("""
+            UPDATE clientes SET 
+                nombre = '"""+nombre+"""',
+                apellidos = '"""+apellidos+"""',
+                email = '"""+email+"""'
+                WHERE identificador = """ +identificador+ """
+            );
+            """)
+    elif opcion == 4:
+        identificador = input("Introduce el identificador a eliminar: ")
+        cursor.execute("""
+            DELETE FROM clientes WHERE identificador = """+identificador+"""
+            ;""")
+    elif opcion == 5:
+        print("Suerte Bro")
+        exit()
+
         
